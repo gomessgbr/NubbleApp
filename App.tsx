553a3129a1e23ad863fbaeab2ea90e6323056b70
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-16 15:10:14 -0300

    feat: create theme and provider on root

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import {Button} from './src/components/Button/Button';
+
 import {Text} from './src/components/Text/Text';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text>Texto Qualquer</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge" style={{fontFamily: 'Satoshi-BlackItalic'}}>
+          Texto Qualquer
+        </Text>
+        <Button title="Zica" />
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
