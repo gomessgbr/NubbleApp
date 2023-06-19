8d4f5970c2e0325c583287c67bcf3c448fa432ae
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-18 23:20:07 -0300

    feat: button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,44 @@
-import {useTheme} from '@shopify/restyle';
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
-import {Theme} from '../../theme/theme';
-import {Text} from '../Text/Text';
 
-interface ButtonProps {
+import {ActivityIndicator} from '../ActivityIndicator/activityIndicator';
+import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
+import {Text} from '../Text/Text';
+import {buttonPresets} from './buttonPresets';
+
+export type ButtonPreset = 'primary' | 'outline';
+
+interface ButtonProps extends TouchableOpacityBoxProps {
   title: string;
+  loading?: boolean;
+  preset?: ButtonPreset;
+  disabled?: boolean;
 }
 
-export function Button({title}: ButtonProps) {
-  const {colors} = useTheme<Theme>();
+export function Button({
+  title,
+  loading,
+  preset = 'primary',
+  disabled,
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
+  const buttonPreset = buttonPresets[preset][disabled ? 'disabled' : 'default'];
   return (
-    <TouchableOpacity>
-      <Text preset="paragraphMedium" bold style={{color: '#fff'}}>
-        {title}
-      </Text>
-    </TouchableOpacity>
+    <TouchableOpacityBox
+      disabled={disabled || loading}
+      paddingHorizontal="s20"
+      height={50}
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="s16"
+      {...buttonPreset.container}
+      {...touchableOpacityBoxProps}>
+      {loading ? (
+        <ActivityIndicator color={buttonPreset.content} />
+      ) : (
+        <Text preset="paragraphMedium" bold color={buttonPreset.content}>
+          {title}
+        </Text>
+      )}
+    </TouchableOpacityBox>
   );
 }
