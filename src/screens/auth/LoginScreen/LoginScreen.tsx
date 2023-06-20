62e33ccbcece69a262ec30c772dde0ccfe8ec0c7
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-19 23:37:55 -0300

    feat: login screen

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,39 +1,41 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-import {Box} from '../../../components/Box/Box';
+
 import {Button} from '../../../components/Button/Button';
+
+import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
+import {Screen} from '../../../components/Screen/Screen';
 import {Text} from '../../../components/Text/Text';
 import {TextInput} from '../../../components/TextInput/TextInput';
 
 export function LoginScreen() {
   return (
-    <SafeAreaView>
-      <View style={{paddingHorizontal: 24}}>
-        <Text marginBottom="s8" preset="headingLarge">
-          Olá
-        </Text>
-        <Text preset="paragraphLarge" mb="s40">
-          Digite seu e-mail e senha para entrar
-        </Text>
+    <Screen scrollable>
+      <Text marginBottom="s8" preset="headingLarge">
+        Olá
+      </Text>
+      <Text preset="paragraphLarge" mb="s40">
+        Digite seu e-mail e senha para entrar
+      </Text>
 
-        <Box mb="s20">
-          <TextInput
-            errorMessage="mensagem de error"
-            label="E-mail"
-            placeholder="Digite seu e-mail"
-          />
-        </Box>
-        <Box>
-          <TextInput label="Senha" placeholder="Digite sua senha" />
-        </Box>
+      <TextInput
+        errorMessage="mensagem de error"
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{mb: 's20'}}
+      />
 
-        <Text color="primary" preset="paragraphSmall" bold mt="s10">
-          Esqueci minha senha
-        </Text>
+      <PasswordInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        boxProps={{mb: 's10'}}
+      />
 
-        <Button marginTop="s48" title="Entrar" />
-        <Button preset="outline" marginTop="s12" title="Criar uma conta" />
-      </View>
-    </SafeAreaView>
+      <Text color="primary" preset="paragraphSmall" bold>
+        Esqueci minha senha
+      </Text>
+
+      <Button marginTop="s48" title="Entrar" />
+      <Button preset="outline" marginTop="s12" title="Criar uma conta" />
+    </Screen>
   );
 }
