ac7eaca19e081d90198af991dd11c024c6994d08
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-20 00:34:58 -0300

    feat: sucess params types

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,13 +3,19 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {IconProp} from '../components/Icon/Icon';
 import {LoginScreen} from '../screens/auth/LoginScreen/LoginScreen';
 import {SignUpScreen} from '../screens/auth/SignUpScreen/SignUpScreen';
+import {SuccessScreen} from '../screens/auth/SuccessScreen/SuccessScreen';
 
-type RootStackParamList = {
+export type RootStackParamList = {
   LoginScreen: undefined;
   SignUpScreen: undefined;
-  SuccessScreen: undefined;
+  SuccessScreen: {
+    title: string;
+    description: string;
+    icon: Pick<IconProp, 'name' | 'color'>;
+  };
 };
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -24,6 +30,7 @@ export function Router() {
         initialRouteName="LoginScreen">
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+        <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
