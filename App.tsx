7b6052694b4bf55ab20fb62da5004c21bd949df9
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-20 00:05:25 -0300

    feat: routers on app root

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {Router} from './src/routes/routes';
 import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <LoginScreen />
+        <Router />
       </ThemeProvider>
     </SafeAreaProvider>
   );
