b32e6f00362104fdb4c480eda3084cab094a3921
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-19 23:15:17 -0300

    feat: showTextinput component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,43 @@
 import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Button} from './src/components/Button/Button';
 
+import {Box} from './src/components/Box/Box';
 import {Text} from './src/components/Text/Text';
+import {TextInput} from './src/components/TextInput/TextInput';
 import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <Text preset="headingLarge" style={{fontFamily: 'Satoshi-BlackItalic'}}>
-          Texto Qualquer
-        </Text>
-        <Button title="Zica" />
+        <View style={{paddingHorizontal: 24}}>
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+
+          <Box mb="s20">
+            <TextInput
+              errorMessage="mensagem de error"
+              label="E-mail"
+              placeholder="Digite seu e-mail"
+            />
+          </Box>
+          <Box>
+            <TextInput label="Senha" placeholder="Digite sua senha" />
+          </Box>
+
+          <Text color="primary" preset="paragraphSmall" bold mt="s10">
+            Esqueci minha senha
+          </Text>
+
+          <Button marginTop="s48" title="Entrar" />
+          <Button preset="outline" marginTop="s12" title="Criar uma conta" />
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   );
