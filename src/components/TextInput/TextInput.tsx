ad9a37c3cb1d95f0cf12155bdb183c22c3b88a1a
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-24 08:18:00 -0300

    fix: full cycle warning

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,4 +1,3 @@
-import {$fontFamily, $fontSizes, Box, BoxProps, Text} from '@components';
 import {useAppTheme} from '@hooks';
 import React, {useRef} from 'react';
 import {
@@ -7,6 +6,8 @@ import {
   TextInputProps as RNTextInputProps,
   TextStyle,
 } from 'react-native';
+import {Box, BoxProps} from '../Box/Box';
+import {$fontFamily, $fontSizes, Text} from '../Text/Text';
 
 export interface TextInputProps extends RNTextInputProps {
   label: string;
