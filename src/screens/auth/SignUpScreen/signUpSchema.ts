91c06a8e9f56993c580de079c6f7c477bf70f3a9
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-24 06:43:34 -0300

    feat: transform submit signup form

--- src/screens/auth/SignUpScreen/signUpSchema.ts
+++ src/screens/auth/SignUpScreen/signUpSchema.ts
@@ -1,10 +1,19 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
 const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
 
 export const signUpSchema = z.object({
-  username: z.string().regex(userNameRegex, 'username inválido'),
-  fullName: z.string().min(5, 'nome muito curto').max(50, 'nome muito longo'),
+  username: z.string().regex(userNameRegex, 'username inválido').toLowerCase(),
+  fullName: z
+    .string()
+    .min(5, 'nome muito curto')
+    .max(50, 'nome muito longo')
+    .transform(value => {
+      return value
+        .split('')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
+    }),
   email: z.string().email('email inválido'),
   password: z.string().min(8, 'password inválido'),
 });
