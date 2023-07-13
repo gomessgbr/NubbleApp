2f42a06ab44720de4829efd2b840977d92263012
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-07-13 19:06:19 -0300

    feat: render images with flatlist

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,15 @@
 import React, {useEffect, useState} from 'react';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  ListRenderItem,
+  ListRenderItemInfo,
+} from 'react-native';
 
 import {Post, postService} from '@domain';
 
-import {Screen, Text} from '@components';
+import {Box, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
 
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
@@ -12,11 +19,35 @@ export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
     postService.getList().then(list => setPostList(list));
   }, []);
 
+  function renderItem({item}: ListRenderItemInfo<Post>) {
+    return (
+      <Box marginBottom="s24">
+        <Box flexDirection="row">
+          <Image
+            source={{uri: item.author.profileURL}}
+            style={{width: 40, height: 40}}
+          />
+          <Text>{item.author.userName}</Text>
+        </Box>
+        <Image
+          source={{uri: item.imageURL}}
+          resizeMode="cover"
+          style={{
+            width: Dimensions.get('screen').width,
+            height: 300,
+          }}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Screen>
-      {postList.map(post => (
-        <Text>{post.text}</Text>
-      ))}
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
     </Screen>
   );
 }
