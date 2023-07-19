cbc813e5822792f79e41ea2b4807a47d084386e5
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-07-19 11:28:00 -0300

    feat: home header component

---
+++ src/screens/app/HomeScreen/components/HomeHeader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {Box, BoxProps, Icon} from '@components';
+import {useAppSafeArea} from '@hooks';
+
+export function HomeHeader() {
+  const {top} = useAppSafeArea();
+  return (
+    <Box {...$wrapper} style={{paddingTop: top}}>
+      <Box backgroundColor="carrotSecondary" height={16} width={70} />
+      <Box flexDirection="row">
+        <Box mr="s24">
+          <Icon name="search" size={16} />
+        </Box>
+        <Box mr="s24">
+          <Icon name="bell" size={16} />
+        </Box>
+        <Icon name="comment" size={16} />
+      </Box>
+    </Box>
+  );
+}
+
+const $wrapper: BoxProps = {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  paddingHorizontal: 's24',
+  paddingBottom: 's24',
+};
