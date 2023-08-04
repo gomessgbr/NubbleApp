c575e31e7d20ccf0e9afaae95fb02a8107d4e7f4
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-08-04 06:13:54 -0300

    feat: change token

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer Nw.p02BLcMHmwZRoL2R4BEOj4dYjatEU_wAMYwSWkf30dPd0aMAzVtZlB-DefWH',
+      'Bearer NQ.albHXJrxEPW85uKevW5S1qWNvA-CV53nr7-mBjRMt96dsDkSPo2_7pIOj0bf',
   },
 });
