5b486b4d3e25a4826c6603f98c5c82d930f8578e
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-08-13 17:04:56 -0300

    feat: header screen component

--- src/components/Screen/components/ScreenHeader.tsx
+++ src/components/Screen/components/ScreenHeader.tsx
@@ -6,28 +6,32 @@ import {Box, Icon, Text, TouchableOpacityBox} from '@components';
 
 import {ScreenProps} from '../Screen';
 
+const ICON_SIZE = 20;
+
 type Props = Pick<ScreenProps, 'title' | 'canGoBack'>;
 export function ScreenHeader({canGoBack, title}: Props) {
   const navigation = useNavigation();
   return (
-    <Box>
+    <Box
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+      mb="s24">
       {canGoBack && (
         <TouchableOpacityBox
           onPress={navigation.goBack}
-          mb="s24"
           flexDirection="row"
-          alignItems="center"
-          justifyContent="space-between">
-          <Icon name="arrowLeft" color="primary" />
+          alignItems="center">
+          <Icon size={ICON_SIZE} name="arrowLeft" color="primary" />
           {!title && (
             <Text preset="paragraphMedium" semiBold ml="s8">
               Voltar
             </Text>
           )}
-          {title && <Text preset="headingSmall">{title}</Text>}
-          {title && <Box width={20} />}
         </TouchableOpacityBox>
       )}
+      {title && <Text preset="headingSmall">{title}</Text>}
+      {title && <Box width={ICON_SIZE} />}
     </Box>
   );
 }
