89ee3e080501d66beb7212ff0fa1763d71279d80
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-09-16 19:28:18 -0300

    refactor: using mutation from react query

--- src/domain/PostComment/useCases/usePostCommentCreate.ts
+++ src/domain/PostComment/useCases/usePostCommentCreate.ts
@@ -1,5 +1,6 @@
 import {PostComment} from '@domain';
-import {useMutation, MutationOptions} from '@infra';
+import {MutationOptions, QueryKeys} from '@infra';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 
 import {postCommentService} from '../postCommentService';
 
@@ -7,17 +8,35 @@ export function usePostCommentCreate(
   postId: number,
   options?: MutationOptions<PostComment>,
 ) {
-  const {mutate, loading, error} = useMutation<{message: string}, PostComment>(
-    ({message}) => postCommentService.create(postId, message),
-    options,
-  );
+  const queryClient = useQueryClient();
+  const {mutate, isLoading, isError} = useMutation<
+    PostComment,
+    unknown,
+    {message: string}
+  >({
+    mutationFn: ({message}) => postCommentService.create(postId, message),
+    onSuccess: data => {
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.PostCommentList, postId],
+      });
+      if (options?.onSuccess) {
+        options.onSuccess(data);
+      }
+    },
+    onError: () => {
+      if (options?.onError) {
+        options.onError(options.errorMessage || 'ocorreu um erro');
+      }
+    },
+  });
+
   async function createComment(message: string) {
-    await mutate({message});
+    mutate({message});
   }
 
   return {
     createComment,
-    loading,
-    error,
+    isLoading,
+    isError,
   };
 }
--- src/domain/PostComment/useCases/usePostCommentRemove.ts
+++ src/domain/PostComment/useCases/usePostCommentRemove.ts
@@ -1,9 +1,30 @@
 import {postCommentService} from '@domain';
-import {MutationOptions, useMutation} from '@infra';
+import {MutationOptions, QueryKeys} from '@infra';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 
-export function usePostCommentRemove(options?: MutationOptions<string>) {
-  return useMutation<{postCommentId: number}, string>(
-    ({postCommentId}) => postCommentService.remove(postCommentId),
-    options,
-  );
+export function usePostCommentRemove(
+  postId: number,
+  options?: MutationOptions<string>,
+) {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<string, unknown, {postCommentId: number}>({
+    mutationFn: ({postCommentId}) => postCommentService.remove(postCommentId),
+    onSuccess: data => {
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.PostCommentList, postId],
+      });
+      if (options?.onSuccess) {
+        options.onSuccess(data);
+      }
+    },
+    onError: () => {
+      if (options?.onError) {
+        options.onError(options.errorMessage || 'ocorreu um erro');
+      }
+    },
+  });
+
+  return {
+    mutate: mutation.mutate,
+  };
 }
