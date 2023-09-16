4fdd5e296a12dd6ccf907c0fb8a803f0a5307e51
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-09-15 23:48:12 -0300

    feat: add tanstack query lib

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import {ToastProvider} from '@services';
+// import {ToastProvider} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
@@ -9,16 +10,22 @@ import {Toast} from '@components';
 import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <ToastProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
+          {/* Only use ToastProvider if it is using Context implementation.
+          Zustand implementation doesn't need it a provider */}
+          {/* <ToastProvider> */}
           <Router />
           <Toast />
-        </ToastProvider>
-      </ThemeProvider>
-    </SafeAreaProvider>
+          {/* </ToastProvider> */}
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
 
