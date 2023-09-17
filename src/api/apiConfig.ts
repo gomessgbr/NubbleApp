7414c9c5312dc9e2f598296fa7c7f5c36eeda2d7
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-09-16 22:58:20 -0300

    refactor: change port

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333/',
+  baseURL: 'http://127.0.0.1:63206/',
   headers: {
     Authorization:
       'Bearer NQ.obPIRJlPw2wEsqbXbwaGby8O-fOZszQpAkSBbZCu7biqbwCkIaG4csdXISDk',
