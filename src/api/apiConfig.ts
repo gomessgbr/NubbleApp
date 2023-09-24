931ca05b41cca89b3de4eca030a064ef42c48c6b
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-09-23 22:56:57 -0300

    feat: create credentials and send to bearer token axios

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:63206/',
-  headers: {
-    Authorization:
-      'Bearer NQ.obPIRJlPw2wEsqbXbwaGby8O-fOZszQpAkSBbZCu7biqbwCkIaG4csdXISDk',
-  },
+  baseURL: 'http://127.0.0.1:51445/',
 });
