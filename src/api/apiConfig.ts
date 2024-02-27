d8e8dca41c3b0c7a5a651055d1cc0c1fa0083285
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-02-27 09:10:37 -0300

    feat: custom render for testing

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,7 +2,7 @@ import {AuthCredentials, authService} from '@domain';
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:50115/',
+  baseURL: 'http://127.0.0.1:3333/',
 });
 
 type InterceptorProps = {
