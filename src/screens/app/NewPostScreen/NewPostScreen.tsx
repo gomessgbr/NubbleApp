84a2708a10b40797b842501f89359296394ff316
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-29 15:04:12 -0300

    feat: render images from camera roll

--- src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Image} from 'react-native';
+
+import {useCameraRoll} from '@services';
 
 import {Screen, Text} from '@components';
 
 export function NewPostScreen() {
+  let {list} = useCameraRoll();
   return (
-    <Screen>
+    <Screen scrollable>
       <Text preset="headingSmall">New Post Screen</Text>
+      {list.map(photo => (
+        <Image
+          key={photo}
+          source={{uri: photo}}
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{width: 200, height: 200}}
+        />
+      ))}
     </Screen>
   );
 }
