9335c76e7011bbad7ff59be97dcbbb978c87837b
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-29 15:19:57 -0300

    refactor: horizontal gallery list

--- src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
 import {useCameraRoll} from '@services';
 
-import {Screen, Text} from '@components';
+import {Screen} from '@components';
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const NUM_COLUMNS = 4;
+
+const ITEM_WIDTH = SCREEN_WIDTH / NUM_COLUMNS;
 
 export function NewPostScreen() {
   let {list} = useCameraRoll();
+
+  function renderItem({item}: ListRenderItemInfo<string>) {
+    return (
+      <Image
+        key={item}
+        source={{uri: item}}
+        style={{width: ITEM_WIDTH, height: ITEM_WIDTH}}
+      />
+    );
+  }
   return (
-    <Screen scrollable>
-      <Text preset="headingSmall">New Post Screen</Text>
-      {list.map(photo => (
-        <Image
-          key={photo}
-          source={{uri: photo}}
-          // eslint-disable-next-line react-native/no-inline-styles
-          style={{width: 200, height: 200}}
-        />
-      ))}
+    <Screen canGoBack noPaddingHorizontal title="Novo post">
+      <FlatList numColumns={NUM_COLUMNS} data={list} renderItem={renderItem} />
     </Screen>
   );
 }
