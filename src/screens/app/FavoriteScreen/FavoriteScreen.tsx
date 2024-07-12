97518a1996fabc2390a80029c1f2cb0ebd83f4af
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-12 00:51:26 -0300

    wip: favorite screen

--- src/screens/app/FavoriteScreen/FavoriteScreen.tsx
+++ src/screens/app/FavoriteScreen/FavoriteScreen.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import {Image, ListRenderItemInfo} from 'react-native';
 
-import {Screen, Text} from '@components';
+import {PostReaction, postReactionService} from '@domain';
+import {QueryKeys} from '@infra';
+
+import {InfinityScrollList, Screen, Text} from '@components';
 
 export function FavoriteScreen() {
+  function renderItem({item}: ListRenderItemInfo<PostReaction>) {
+    return (
+      <Image
+        source={{uri: item.post.imageURL}}
+        style={{width: 300, height: 300}}
+      />
+    );
+  }
   return (
-    <Screen>
+    <Screen flex={1}>
       <Text preset="headingSmall"> FavoriteScreen</Text>
+      <InfinityScrollList
+        queryKey={QueryKeys.FavoriteList}
+        getList={page => postReactionService.getMyReactions('favorite', page)}
+        renderItem={renderItem}
+        flatListProps={{}}
+        emptyListProps={{
+          emptyMessage: 'não há favoritos',
+          errorMessage: 'erro ao carregar favoritos',
+        }}
+      />
     </Screen>
   );
 }
