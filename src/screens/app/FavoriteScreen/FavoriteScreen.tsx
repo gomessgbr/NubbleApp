833e8fe7a8229de761067705c484fb1ec68d9812
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-12 01:09:39 -0300

    feat: render item for favorite screen

--- src/screens/app/FavoriteScreen/FavoriteScreen.tsx
+++ src/screens/app/FavoriteScreen/FavoriteScreen.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import {Image, ListRenderItemInfo} from 'react-native';
+import {Dimensions, Image, ListRenderItemInfo} from 'react-native';
 
 import {PostReaction, postReactionService} from '@domain';
 import {QueryKeys} from '@infra';
 
-import {InfinityScrollList, Screen, Text} from '@components';
+import {Box, InfinityScrollList, Screen, Text} from '@components';
+
+const NUM_COLUMNS = 2;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_PADDING = 24;
+const ITEM_MARGIN = 16;
+
+const ITEM_WITH =
+  (SCREEN_WIDTH - ITEM_MARGIN - SCREEN_PADDING * 2) / NUM_COLUMNS;
 
 export function FavoriteScreen() {
   function renderItem({item}: ListRenderItemInfo<PostReaction>) {
     return (
-      <Image
-        source={{uri: item.post.imageURL}}
-        style={{width: 300, height: 300}}
-      />
+      <Box>
+        <Image
+          source={{uri: item.post.imageURL}}
+          style={{width: ITEM_WITH, height: ITEM_WITH}}
+        />
+        <Text semiBold>{item.author.username}</Text>
+      </Box>
     );
   }
   return (
@@ -22,7 +33,11 @@ export function FavoriteScreen() {
         queryKey={QueryKeys.FavoriteList}
         getList={page => postReactionService.getMyReactions('favorite', page)}
         renderItem={renderItem}
-        flatListProps={{}}
+        flatListProps={{
+          numColumns: NUM_COLUMNS,
+          columnWrapperStyle: {columnGap: ITEM_MARGIN},
+          contentContainerStyle: {rowGap: SCREEN_PADDING},
+        }}
         emptyListProps={{
           emptyMessage: 'não há favoritos',
           errorMessage: 'erro ao carregar favoritos',
