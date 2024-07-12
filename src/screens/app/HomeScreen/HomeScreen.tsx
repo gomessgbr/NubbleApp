1783eaaddac986f0bf4ef4228999d435cf6e810b
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-12 01:02:04 -0300

    refactor: home screen with infinite screen component

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,55 +1,26 @@
 import React from 'react';
-import {
-  FlatList,
-  ListRenderItemInfo,
-  RefreshControl,
-  StyleProp,
-  ViewStyle,
-} from 'react-native';
+import {ListRenderItemInfo, StyleProp, ViewStyle} from 'react-native';
 
-import {Post, usePostList} from '@domain';
-import {useScrollToTop} from '@react-navigation/native';
+import {Post, postService} from '@domain';
+import {QueryKeys} from '@infra';
 
-import {PostItem, Screen} from '@components';
+import {InfinityScrollList, PostItem, Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
 
-import {HomeEmpty} from './components/HomeEmpty';
 import {HomeHeader} from './components/HomeHeader';
 
 export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
-  const {
-    list: postList,
-    isError,
-    isLoading,
-    refresh,
-    fetchNextPage,
-  } = usePostList();
-
-  const flatListRef = React.useRef<FlatList>(null);
-  useScrollToTop(flatListRef);
   function renderItem({item}: ListRenderItemInfo<Post>) {
     return <PostItem post={item} />;
   }
 
   return (
     <Screen style={$screen}>
-      <FlatList
-        ref={flatListRef}
-        showsVerticalScrollIndicator={false}
-        data={postList}
-        keyExtractor={item => item.id.toString()}
+      <InfinityScrollList
         renderItem={renderItem}
-        onEndReached={fetchNextPage}
-        onEndReachedThreshold={0.1}
-        contentContainerStyle={{flex: postList.length === 0 ? 1 : undefined}}
-        refreshControl={
-          <RefreshControl refreshing={isLoading} onRefresh={refresh} />
-        }
-        refreshing={isLoading}
-        ListHeaderComponent={<HomeHeader />}
-        ListEmptyComponent={
-          <HomeEmpty refetch={refresh} error={isError} loading={isLoading} />
-        }
+        queryKey={QueryKeys.PostList}
+        getList={postService.getList}
+        flatListProps={{ListHeaderComponent: <HomeHeader />}}
       />
     </Screen>
   );
