a8b81f95fae2efebff604cd58542a449bd637d4b
Gabriel Gomes <gabrielgomes98@gmail.com>
2025-01-23 15:38:00 -0300

    fix: build android metro expo error

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
 
+const {mergeConfig} = require('@react-native/metro-config');
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
